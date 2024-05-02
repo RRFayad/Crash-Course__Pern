88839cd36c7547fbc79c6bfff955024c55727e8f
RRFayad <renan.r.fayad@gmail.com>
2024-05-02 09:54:13 -0300

    feat(api): implemented post todo route

--- api/index.js
+++ api/index.js
@@ -12,6 +12,20 @@ app.use(express.json()); // Gives us access to the req body
 
 // create a todo
 
+app.post("/todos", async (req, res, next) => {
+  try {
+    const { description } = req.body;
+
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *", // RETURNING * for returning data inside a rows array
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 // get all todos
 
 // get a todo
