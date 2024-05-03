8f3cf86de4ca91825c7337f75325bc71feac618a
RRFayad <renan.r.fayad@gmail.com>
2024-05-03 08:19:20 -0300

    feat(api): implemented get todo route and controller

--- api/index.js
+++ api/index.js
@@ -37,6 +37,19 @@ app.get("/todos", async (req, res, next) => {
 });
 
 // get a todo
+app.get("/todos/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const todo = await pool.query(
+      "SELECT * FROM todo WHERE id = ($1)", // RETURNING * for returning data inside a rows array
+      [id]
+    );
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // update a todo
 
